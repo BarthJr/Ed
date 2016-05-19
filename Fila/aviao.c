0d43a41ff5f976b7af03c52ec93c9aa07847b7f2
oliveiraCynthia <cynthiarochaoliver@outlook.com>
2016-05-19 00:04:21 -0300

    Update aviao.c
    Agora está certo o arquivo

--- Fila/aviao.c
+++ Fila/aviao.c
@@ -2,25 +2,24 @@
 #include<stdlib.h>
 #include<string.h>
 
-//Fila: Insere no final e remove do inÌcio.
+//Fila: Insere no final e remove do in√≠cio.
 
-/*FaÁa um programa em C para simular um controlador de voo de um aeroporto. Neste programa o
-usu·rio deve ser capaz de realizar as seguintes tarefas:
-ï Listar o n˙mero de aviıes esperando para decolar; -> Percorrer a fila de decolagem e fazer um count.
-ï Listar o n˙mero de aviıes esperando para pousar; -> Percorrer a fila de pouso e fazer um count.
-ï Autorizar a decolagem do primeiro avi„o na fila de decolagem; -> Remove inÌcio fila decolagem.
-ï Autorizar o pouso do primeiro avi„o na fila de pouso; -> Remove inÌcio fila pouso.
-ï Adicionar um avi„o na fila de espera para decolagem; -> Inserir final decolagem.
-ï Adicionar um avi„o na fila de espera para pouso; -> Inserir final pouso.
-ï Listar todos os aviıes que est„o na fila de espera para decolagem, por ordem de chegada;  -> Percorrer toda a fila de decolagem.
-ï Listar todos os aviıes que est„o na fila de espera para pouso, por ordem de chegada; -> Percorrer toda a fila de pouso.
-ï Listar as caracterÌsticas do primeiro avi„o da fila, antes de autorizar a sua decolagem; -> Dar um print nos dados armazenados no primeiro nodo da fila.
-ï Listar as caracterÌsticas do primeiro avi„o da fila, antes de autorizar o seu pouso; -> Dar um print nos dados armazenados no primeiro nodo da fila. */
+/*Fa√ßa um programa em C para simular um controlador de voo de um aeroporto. Neste programa o
+usu√°rio deve ser capaz de realizar as seguintes tarefas:
+‚Ä¢ Listar o n√∫mero de avi√µes esperando para decolar; -> Percorrer a fila de decolagem e fazer um count.
+‚Ä¢ Listar o n√∫mero de avi√µes esperando para pousar; -> Percorrer a fila de pouso e fazer um count.
+‚Ä¢ Autorizar a decolagem do primeiro avi√£o na fila de decolagem; -> Remove in√≠cio fila decolagem.
+‚Ä¢ Autorizar o pouso do primeiro avi√£o na fila de pouso; -> Remove in√≠cio fila pouso.
+‚Ä¢ Adicionar um avi√£o na fila de espera para decolagem; -> Inserir final decolagem.
+‚Ä¢ Adicionar um avi√£o na fila de espera para pouso; -> Inserir final pouso.
+‚Ä¢ Listar todos os avi√µes que est√£o na fila de espera para decolagem, por ordem de chegada;  -> Percorrer toda a fila de decolagem.
+‚Ä¢ Listar todos os avi√µes que est√£o na fila de espera para pouso, por ordem de chegada; -> Percorrer toda a fila de pouso.
+‚Ä¢ Listar as caracter√≠sticas do primeiro avi√£o da fila, antes de autorizar a sua decolagem; -> Dar um print nos dados armazenados no primeiro nodo da fila.
+‚Ä¢ Listar as caracter√≠sticas do primeiro avi√£o da fila, antes de autorizar o seu pouso; -> Dar um print nos dados armazenados no primeiro nodo da fila. */
 
-/* Cada avi„o possui um nome, um identificador, uma origem e um destino. Se quiser coloque
-mais informaÁıes, n˙mero de passageiros, capacidade, modelo, etc. */
+/* Cada avi√£o possui um nome, um identificador, uma origem e um destino. Se quiser coloque
+mais informa√ß√µes, n√∫mero de passageiros, capacidade, modelo, etc. */
 
-/*int eita;*/
 
 typedef struct Aviao{
     int identificador;
@@ -34,41 +33,44 @@ void inicializa_fila(Nodo **N){//inicializa a lista
     *N = NULL;
 }
 
-Nodo * Cria_Aviao(){//aloca memÛria para o nodo
+Nodo * Cria_Aviao(){//aloca mem√≥ria para o nodo
     Nodo *p;
     p = (Nodo *) malloc(sizeof(Nodo));
     if(!p){
-        printf("Problema de alocaÁ„o");
+        printf("Problema de aloca√ß√£o");
         exit(0);
     }
     return p;
 }
 
-void insere_fim_fila(Nodo **N, int id){ //Passar os par‚metros de texto com CHAR NOME[30]
-    Nodo *novo, *aux;
-    novo = Cria_Aviao( );
-    novo->dado = dado;
-    novo->prox = NULL;
-    if(*N == NULL)
-        *N = novo;
+void insere_fim_fila(Nodo **N, int id, char nome[30], char origem[40], char destino[40]){
+    Nodo *novo_aviao, *aux;
+    novo_aviao = Cria_Aviao( );
+    novo_aviao->identificador = id;
+    strcpy(novo_aviao->nome, nome); //Primeiro parametro √© onde vai armazenar e o segundo √© o dado a ser inserido.
+    strcpy(novo_aviao->origem, origem);
+    strcpy(novo_aviao->destino, destino);
+    novo_aviao->prox = NULL;
+    if(*N == NULL) //Se a lista estiver vazia.
+        *N = novo_aviao; //Esse vai ser o primeiro endere√ßo.
     else{
         aux = *N;
         while(aux->prox != NULL)
             aux = aux->prox;
-        aux->prox = novo;
-    }
+        aux->prox = novo_aviao;
+    } //N√£o precisa mudar o N porque ele sempre aponta para o PRIMEIRO endere√ßo e aqui ta sendo inserido o √∫ltimo.
 }
 
 
-int remove_inicio_fila(Nodo **N, int *dado){
+int remove_inicio_fila(Nodo **N, int *id){
     Nodo *aux;
-    if(*N == NULL) //verifica se a lista est· vazia
+    if(*N == NULL) //verifica se a lista est√° vazia
         return 0;
     else{
-        *dado = (*N)->dado;
+        *id = (*N)->identificador;
         aux = (*N)->prox;
         free(*N);
-        *N = aux;
+        *N = aux; //Tem o novo aviao a decolr/pousar
     }
     return 1;
 }
@@ -77,92 +79,203 @@ int remove_inicio_fila(Nodo **N, int *dado){
 void imprime_fila(Nodo *N){
     Nodo *aux;
     if(N == NULL)
-        printf("\n A lista est· vazia!!");
+        printf("\n\nNao ha nenhum aviao na fila!");
     else{
         for(aux = N; aux != NULL; aux = aux->prox)
-            printf("\n%d", aux->dado);
+            printf("\n\n   AVIAO %d:\n\nIdentificador: %d \nNome: %s \nOrigem: %s \nDestino: %s", aux->identificador, aux->identificador, aux->nome, aux->origem, aux->destino);
     }
 }
 
 int conta_aviao(Nodo *N){
     Nodo *aux;
     int cont=0;
-    if(N == NULL) //A fila est· vazia.
+    if(N == NULL) //A fila est√° vazia.
         return 0;
     else{//Se a fila possui NODOS
-        for(aux = N; aux->prox != NULL; aux = aux->prox) //ComeÁa do primeiro endereÁo enquanto prox diferente de NULL (fim da fila), indo sempre para posicao armazenada no prox
+        for(aux = N; aux != NULL; aux = aux->prox) //Come√ßa do primeiro endere√ßo enquanto prox diferente de NULL (fim da fila), indo sempre para posicao armazenada no prox
             cont++; //Contador para saber quantos elementos tem.
     }
     return cont;
 }
 
+void imprime_aviao(Nodo *N){
+    Nodo *aviao;
+    aviao = N;
+    if(N == NULL)
+        printf("\n");
+    else
+        printf("\n\nIdentificador: %d \nNome: %s \nOrigem: %s \nDestino: %s", aviao->identificador, aviao->nome, aviao->origem, aviao->destino);
+}
+
+void desenha_decolagem(){
+              printf("\n\n   __|__              __|__ ");
+                printf("\n*---oOo---*        *---oOo---*  ");
+                printf("\n_____________________________________*_*_*_*___");
+				printf("\n\n"); 	
+}
+
+void desenha_pouso(){
+	          printf("\n\n                   _ .");                  
+                printf("\n                 (  _ )_");
+                printf("\n                (_  _(_ ,)"); 
+                printf("\n        __|__");   
+                printf("\n     *---oOo---*");    
+                printf("\n"); 
+                printf("\n     _ ."); 
+                printf("\n   (  _ )_");
+                printf("\n (_  _(_ ,)"); 
+                printf("\n\n");
+}
+
+void desenha_nuvem(){
+	printf("\n                   _ .");                  
+    printf("\n                 (  _ )_");
+    printf("\n                (_  _(_ ,)"); 
+    printf("\n");
+    printf("\n"); 
+    printf("\n     _ ."); 
+    printf("\n   (  _ )_");
+    printf("\n (_  _(_ ,)");
+    printf("\n\n\n\n\n____*_*_*_*__________*_*_*_*_________*_*_*_*___");
+}
+
 int main(){
     Nodo *Decolagem;
     Nodo *Pouso;
-    int menu, id, resul;
+    int menu, id, cont, resp;
+    char nome[30], origem[40], destino[40];
     inicializa_fila(&Decolagem);
     inicializa_fila(&Pouso);
     do{
-        printf("\n1. Adicionar aviao na fila para decolagem");
+    	system("cls");
+    	printf("\n                     __|__");
+printf("\n            __|__ *---oOo---*"); 
+printf("\n   __|__ *---oOo---*");
+printf("\n*---oOo---*"); 
+printf("\n\n\n");
+    	printf("               CONTROLADOR DE VOO");
+        printf("\n\n1. Adicionar aviao na fila para decolagem");
         printf("\n2. Adicionar aviao na fila para pouso");
         printf("\n3. Ver os avioes esperando para decolar");
         printf("\n4. Ver os avioes esperando para pousar");
         printf("\n5. Quantidade de avioes esperando para decolar");
         printf("\n6. Quantidade de avioes esperando para pousar");
-        printf("\n7. Autorizar a decolagem"); //MOSTRAR AS INFORMA«’ES ANTES DE DECOLAR
-        printf("\n8. Autorizar o pouso"); //MOSTRAR AS INFORMA«’ES ANTES DE POUSAR
-        printf("\n7. Sair\n");
+        printf("\n7. Autorizar a decolagem"); //MOSTRAR AS INFORMA√á√ïES ANTES DE DECOLAR
+        printf("\n8. Autorizar o pouso"); //MOSTRAR AS INFORMA√á√ïES ANTES DE POUSAR
+        printf("\n9. Sair\n");
+        printf("\n\nNumero: ");
         scanf("%d", &menu);
         switch(menu){
-            case 1:
-                printf("\nInforme as seguintes caracteristicas do aviao que ira decolar:");
-                printf("\nNumero identificador:");
-                scanf("%d", &id);
-                printf("\nNome:");
-                scanf("%s", nome); //Nome j· È endereÁo porque È um vetor.
-                printf("\nOrigem:");
-                scanf("%s", origem);
-                printf("\nDestino:");
-                scanf("%s", destino);
+            case 1: //Insere no FIM Decolagem
+                system("cls");
+                printf("\n   AVIAO ESPERANDO PARA DECOLAR:");
+                desenha_decolagem();
+                printf("\nNumero identificador: ");
+                scanf(" %d", &id);
+                getchar(); //Limpa o buffer do scanf
+                printf("\nNome: ");
+                gets(nome); //Nome j√° √© endere√ßo porque √© um vetor.
+                printf("\nOrigem: ");
+                gets(origem);
+                printf("\nDestino: ");
+                gets(destino);
                 insere_fim_fila(&Decolagem, id, nome, origem, destino);
+                printf("\n\n\n\n"); 
+                system("pause");
                 break;
-            case 2:
-                printf("\nInforme o valor a ser inserido no inicio da lista:");
-                scanf("%d", &valor);
-                insere_inicio_lista(&MyList, valor);
+            case 2: //Insere no FIM Pouso
+                system("cls");
+                printf("\n   AVIAO ESPERANDO PARA POUSAR:");
+                desenha_pouso();
+                printf("\n\nNumero identificador: ");
+                scanf("%d", &id);
+                getchar();
+                printf("\nNome: ");
+                gets(nome); //Nome j√° √© endere√ßo porque √© um vetor.
+                printf("\nOrigem: ");
+                gets(origem);
+                printf("\nDestino: ");
+                gets(destino);
+                insere_fim_fila(&Pouso, id, nome, origem, destino);
+                printf("\n\n\n\n"); 
+                system("pause");
                 break;
-            case 3:
-                if(remove_inicio_lista(&MyList,&valor) == 0)
-                    printf("\nA lista est· vazia!!");
-                else
-                    printf("\nO valor excluido do inicio da lista foi: %d", valor);
+            case 3: //Imprime fila Decolagem
+                system("cls");
+                printf("   FILA PARA DECOLAGEM:");
+                imprime_fila(Decolagem);
+                printf("\n\n\n\n"); 
+                system("pause");
                 break;
-            case 4:
-                if(remove_fim_lista(&MyList,&valor) == 0)
-                    printf("\nA lista est· vazia!!");
-                else
-                    printf("\nO valor excluido do fim da lista foi: %d", valor);
+            case 4: //Imprime fila Pouso
+                system("cls");
+                printf("   FILA PARA POUSO:");
+                imprime_fila(Pouso);
+                printf("\n\n\n\n"); 
+                system("pause");
                 break;
-            case 5:
-                printf("\nInforme o valor a ser excluido da lista:");
-                scanf("%d", &valor);
-                resul= remove_elem_lista(&MyList,valor);
-                if(resul==0)
-                    printf("\nA lista est· vazia!!");
-                else if (resul == 1 )
-                    printf("\nO valor %d foi excluido da lista!!",valor);
-                else
-                    printf("\n O valor %d nao foi encontrado",valor);
+            case 5: //Conta aviao Decolagem
+                system("cls");
+                printf("     DECOLAGEM");
+                cont = conta_aviao(Decolagem);
+                if (cont == 0){
+                    desenha_nuvem();
+                    printf("\n\nNao ha avioes esperando para decolar!");
+                }else{
+                    desenha_decolagem();
+                    printf("\n\nO numero de avioes esperando para decolar eh de: %d", cont);
+                }
+				printf("\n\n\n\n"); 
+                system("pause");
                 break;
-            case 6:
-                imprime_lista_ecandeada(MyList);
+            case 6: //Conta avi√£o pouso
+                system("cls"); 
+				printf("     POUSO");            
+                cont = conta_aviao(Pouso);
+                if (cont == 0){
+                    desenha_nuvem();
+                    printf("\n\nNao ha avioes esperando para pousar!");
+                }else{
+                    desenha_pouso();
+                    printf("\n\nO numero de avioes esperando para pousar eh de: %d", cont);
+			    }
+				printf("\n\n\n\n"); 
+                system("pause");
                 break;
-            case 7:
-                printf("\n\n\nSaindo do programa!");
+            case 7: //Imprimir primeiro NODO Decolagem e remove inicio fila Decolagem
+                system("cls");
+                printf("   INICIAR DECOLAGEM");
+				imprime_aviao(Decolagem);
+                if(remove_inicio_fila(&Decolagem,&id) == 0){
+                    desenha_nuvem();
+                    printf("\n\nNao ha ninguem na fila para decolar!");
+                }else{
+                    desenha_decolagem();
+                    printf("\n\nO aviao %d esta decolando ", id);
+                }
+				printf("\n\n\n\n"); 
+                system("pause");
+                break;
+            case 8: //Imprimir primeiro NODO Pouso e remove inicio fila Pouso.
+                system("cls");
+                printf("   INICIAR POUSO");
+				imprime_aviao(Pouso);
+                if(remove_inicio_fila(&Pouso,&id) == 0){
+                    desenha_nuvem();
+					printf("\n\nNao ha ninguem na fila para pousar!");
+                }else{
+                    desenha_pouso();
+                    printf("\n\nO aviao %d esta pousando", id);
+                }
+				printf("\n\n\n\n"); 
+                system("pause");
+                break;
+            case 9: //Sair
+                printf("\n\n\nSaindo do controlador de voo!");
                 break;
             default:
-                printf("\nOpcao Invalida!!!");
+                printf("\nOpcao Invalida!!");
         }
-    }while(menu != 7);
+    }while(menu != 9);
     return 0;
 }
